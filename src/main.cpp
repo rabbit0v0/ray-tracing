664b9bc735bdebdffd50e59c6ed7bcf9c8c25e6d
TracebackS <673622006@qq.com>
2018-11-06 18:06:04 +0800

    Revert "just commit"
    This reverts commit eeb6fbf1c15b7f4f49b449c5853fe2ebcde789c0.

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@
 #include <glm/gtx/matrix_query.hpp>
 #include <utility>
 
-#define DEBUG 1
+#define DEBUG 0
 
 enum VAO_IDs
 {
@@ -27,11 +27,8 @@ enum Attrib_IDs
 	vPosition = 0
 };
 
-GLuint VAOs[NumVAOs];
-GLuint Buffers[NumBuffers];
-
 const GLfloat pi = 3.141591653589793238462643383279502f;
-GLfloat sphere[8][9][2][3];
+GLfloat sphere[200][200][2][3];
 
 glm::mat4 ModelS = glm::mat4(1.0f);
 glm::mat4 ModelR = glm::mat4(1.0f);
@@ -46,6 +43,9 @@ static void APIENTRY simple_print_callback(GLenum source, GLenum type, GLuint id
 	printf("Debug message  '%s'\n", message);
 }
 #endif
+
+GLuint VAOs[NumVAOs];
+GLuint Buffers[NumBuffers];
 
 void init(void);
 void display(void);
@@ -73,7 +73,6 @@ int main()
 		SDL_GL_SwapWindow(window);
 	}
 }
-
 void init(void)
 {
 #if DEBUG
@@ -81,10 +80,8 @@ void init(void)
 	glDebugMessageCallback(simple_print_callback, NULL);
 	glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, NULL, GL_TRUE);
 #endif
-
 	glGenBuffers(NumBuffers, Buffers);
 	glBindBuffer(GL_ARRAY_BUFFER, Buffers[ArrayBuffer]);
-
 	glGenVertexArrays(NumVAOs, VAOs);
 	glBindVertexArray(VAOs[Triangles]);
 	glVertexAttribPointer(vPosition, 3, GL_FLOAT, GL_FALSE, 0, 0);
@@ -94,26 +91,25 @@ void init(void)
 	loadShader("default.frag");
 	useProgram();
 
-	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	glPolygonMode(GL_FRONT_AND_BACK, GL_POINT);
 
-	GLfloat z_last = -1;
-	for (int i = 0; i < 8; i++)
+	GLfloat z_last = -0.7;
+	for (int i = 0; i < 200; i++)
 	{
-		GLfloat z = sin(((i + 1.0f - 4.0f) / 8.0f) * pi);
-		for (int j = 0; j < 9; j++)
+		GLfloat z = 0.7 * sin(((i + 1.0f - 100.0f) / 200.0f) * pi);
+		for (int j = 0; j < 200; j++)
 		{
-			GLfloat x = cos((j / 4.0f) * pi);
-			GLfloat y = sin((j / 4.0f) * pi);
-			sphere[i][j][0][0] = x * cos(((i - 4.0f) / 9.0f) * pi);
-			sphere[i][j][0][2] = y * cos(((i - 4.0f) / 9.0f) * pi);
+			GLfloat x = 0.7 * cos((j / 100.0f) * pi);
+			GLfloat y = 0.7 * sin((j / 100.0f) * pi);
+			sphere[i][j][0][0] = x * cos(((i - 100.0f) / 200.0f) * pi);
+			sphere[i][j][0][2] = y * cos(((i - 100.0f) / 200.0f) * pi);
 			sphere[i][j][0][1] = z_last;
-			sphere[i][j][1][0] = x * cos(((i + 1.0f - 4.0f) / 9.0f) * pi);
-			sphere[i][j][1][2] = y * cos(((i + 1.0f - 4.0f) / 9.0f) * pi);
+			sphere[i][j][1][0] = x * cos(((i + 1.0f - 100.0f) / 200.0f) * pi);
+			sphere[i][j][1][2] = y * cos(((i + 1.0f - 100.0f) / 200.0f) * pi);
 			sphere[i][j][1][1] = z;
 		}
 		z_last = z;
 	}
-	glBufferData(GL_ARRAY_BUFFER, sizeof(sphere), sphere, GL_STATIC_DRAW);
 }
 
 void display(void)
@@ -128,6 +124,12 @@ void display(void)
 	glm::mat4 Model = ModelT * ModelR * ModelS;
 
 	glm::mat4 clip_model = clip_view * View * Model;
+	glBufferData(GL_ARRAY_BUFFER, sizeof(sphere), sphere, GL_STATIC_DRAW);
 	glUniformMatrix4fv(0, 1, GL_FALSE, glm::value_ptr(clip_model));
-	glDrawArrays(GL_TRIANGLE_STRIP, 0, 8 * 9 * 2);
+	glDrawArrays(GL_TRIANGLE_STRIP, 0, 200 * 200 * 2);
+
+	GLfloat axis[6][3] = {{-100.0f, 0.0f, 0.0f}, {100.0f, 0.0f, 0.0f},  {0.0f, -100.0f, 0.0f},
+						  {0.0f, 100.0f, 0.0f},  {0.0f, 0.0f, -100.0f}, {0.0f, 0.0f, 100.0f}};
+	glBufferData(GL_ARRAY_BUFFER, sizeof(axis), axis, GL_STATIC_DRAW);
+	glDrawArrays(GL_LINES, 0, 6);
 }
